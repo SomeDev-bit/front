cd5b4b34df9b63a86cb9cc1966e80a5b429df5d3
SomeDev-bit <batchdevops431@gmail.com>
2025-02-14 07:28:11 +0545

    changes

--- src/app/apiUrl.js
+++ src/app/apiUrl.js
@@ -1,4 +1,6 @@
 
 
-export const baseApi = 'http://192.168.1.73:5000';
-export const baseUrl = 'http://192.168.1.73:5000/api';
+export const baseApi = 'https://backened-fn4j.onrender.com';
+export const baseUrl = 'https://backened-fn4j.onrender.com/api';
+// export const baseApi = 'http://192.168.1.73:5000';
+// export const baseUrl = 'http://192.168.1.73:5000/api';
